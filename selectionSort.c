7834c8eae2638e7cee1555ce9e8fb45bc4543b62
Mukesh-Negi <107361627+Mukesh-Negi@users.noreply.github.com>
2023-02-21 15:00:48 +0530

    program to perform Selection Sort

---
+++ selectionSort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+void selSort(int arr[], int size)
+{
+	int i , j;
+	for( i = 0; i <= size-2; i++)
+	{
+		int minIdx = i;
+		for( j = i+1; j <= size-1; j++)
+		{
+			if( arr[j] < arr[minIdx])
+			{
+				minIdx = j;
+			}
+ 		}
+ 			int temp = arr[i];
+			arr[i] = arr[minIdx];
+			arr[minIdx] = temp;
+	}
+}
+void printArray(int arr[], int size)
+{
+	printf("ELEMENTS OF ARRAY ARE:->");
+	int i;
+	for(i = 0; i < size; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+int main()
+{
+	int n; 
+	printf("ENTER NUMBER OF ELEMENTS :->");
+	scanf("%d", &n);
+	int arr[n];
+	printf("ENTER ELEMENTS OF ARRAY :->");
+	int i = 0;
+	for ( i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	printArray(arr, n);
+	printf("              PERFORMING SELECTION SORT :\n");
+	selSort(arr, n);
+	printArray(arr, n);
+	return 0;
+}
