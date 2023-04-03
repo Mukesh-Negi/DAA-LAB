9a2b918a750900c032c7f7dfa39a5d6c331ccf0b
Mukesh-Negi <107361627+Mukesh-Negi@users.noreply.github.com>
2023-04-03 11:09:28 +0000

    common elements in two sorted arrays

--- commonElem.c
+++ commonElem.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
+void insSort( int *arr, int size)
+{
+	int i;
+	for( i = 1; i < size; i++)
+	{
+		int key = arr[i];
+		int j = i-1;
+		while( j >= 0 )
+		{
+			if( arr[j] > key)
+			{
+				arr[j+1] = arr[j];
+				j--;
+			}
+			else break;
+		}
+		arr[j+1] = key;
+        }
+}
 void common(int arr1[], int arr2[], int m, int n)
 {
     int i = 0; int j = 0;
@@ -23,10 +42,13 @@ void common(int arr1[], int arr2[], int m, int n)
 }
 int main()
 {
+
     int arr1[] = {34, 76, 10, 39, 85, 10, 55};
     int arr2[] = {30, 55, 34, 72, 10, 34, 10, 89, 11, 30, 69, 51};
     int s1 = 7;
     int s2 = 12;
+    insSort(arr1, s1);
+    insSort(arr2, s2);
     common(arr1, arr2, s1, s2);
     return 0;
 }
